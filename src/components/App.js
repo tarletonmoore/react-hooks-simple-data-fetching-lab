22c8cc4804587ef628575ea90e0477e054960f76
John Tarleton Moore <tarletonmoore@gmail.com>
2022-06-13 10:36:56 -0400

    commit tree 932b3714efe76bce13974e074fbc8a673fd92978

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,38 @@
 // create your App component here
+import React, { useState, useEffect } from "react"
+
+function App() {
+    // const [error, setError] = useState(null)
+    const [isLoaded, setIsLoaded] = useState(false)
+    const [images, setImages] = useState([])
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then(response => response.json())
+            .then((result) => {
+                console.log(result.message)
+                setIsLoaded(true)
+                setImages(result.message)
+            },
+                // (error) => {
+                //     setIsLoaded(true)
+                //     setError(error)
+                // }
+            )
+    }, [])
+
+    if (!isLoaded) {
+        return <p>Loading...</p>
+    }
+
+    return (
+        <div>
+            <img src={images} alt="A Random Dog" />
+        </div>
+    )
+
+
+
+
+}
+
+export default App
